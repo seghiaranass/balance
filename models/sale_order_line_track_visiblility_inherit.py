4f0962150ab39ccd5afc27e3edf41a06c16207dd
seghiaranass <anass.segh97@gmail.com>
2023-10-13 11:33:18 +0100

    Add track_visiblity equal to alwyas for all filds in balance module

--- models/sale_order_line_track_visiblility_inherit.py
+++ models/sale_order_line_track_visiblility_inherit.py
@@ -1,6 +1,6 @@
 from odoo import api, models,fields
 
 class SaleOrderLineTrackVisibilityInherit(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = 'sale.order'
 
     product_uom_qty = fields.Float(track_visibility='always')
