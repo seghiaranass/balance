88c265deb24f272a6e763d5c3fe6e173a77fba05
seghiaranass <anass.segh97@gmail.com>
2023-10-13 11:06:01 +0100

    Add track visibility to sale order line using inherit model

--- models/sale_order_line_track_visiblility_inherit.py
+++ models/sale_order_line_track_visiblility_inherit.py
@@ -4,7 +4,7 @@ class SaleOrderLineTrackVisibilityInherit(models.Model):
     _name = "sale_order.line.track.visiblility.inherit"
     _description = "This model only to add track visiblity to sale order line model  "
 
-    _inherit = 'sale.order'
+    _inherit = 'sale.order.line'
 
 
     product_uom_qty = fields.Float(track_visibility='always')
