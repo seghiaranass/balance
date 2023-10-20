89b63c1c21821ca2d02a2675fef4b00a104beb08
seghiaranass <anass.segh97@gmail.com>
2023-10-20 18:09:31 +0100

    Create print button in balance form view

--- __manifest__.py
+++ __manifest__.py
@@ -35,7 +35,7 @@
             'balance/static/src/xml/add_pin.xml',
             # 'balance/static/src/xml/edit_lock_template.xml'
             'balance/static/src/js/edit_lock.js',
-            'balance/static/src/js/notification.js',
+            # 'balance/static/src/js/notification.js',
             # 'balance/static/src/js/add_pin.xml',
             
         ],
